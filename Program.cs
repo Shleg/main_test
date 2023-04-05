1588328fe3865a19fa5b69f5ea211f84242adb50
Andrew Shlegel <a.shlegel@outlook.com>
2023-04-05 10:40:09 -0300

    CreateStringArray function added

--- Program.cs
+++ Program.cs
@@ -10,3 +10,19 @@
     }
 
     return result;
+}
+
+
+string[] CreateStringArray(int length)
+{
+    string[] stringArray = new string[length];
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"Enter {i + 1} string: ");
+        stringArray[i] = Console.ReadLine();
+    }
+
+    return stringArray;
+}
+
+
