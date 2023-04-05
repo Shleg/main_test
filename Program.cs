e8774dc92c4e8628535df74fe61a6aa9304508a5
Andrew Shlegel <a.shlegel@outlook.com>
2023-04-05 10:38:58 -0300

    InputUserData function added

--- Program.cs
+++ Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int InputUserData(string messageToUser)
+{
+    int result;
+    Console.Write(messageToUser);
+    while (!(int.TryParse(Console.ReadLine(), out result) & (result > 0)))
+    {
+        Console.WriteLine("You must enter a positive integer!!!");
+        Console.WriteLine();
+        Console.Write(messageToUser);
+    }
+
+    return result;
